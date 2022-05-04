990454964450df22caf17be606df1951e3be469e
BFSmx5 <97541881+BSFmx5@users.noreply.github.com>
2022-05-04 10:08:29 -0500

    Update organise.py

--- organise.py
+++ organise.py
@@ -9,7 +9,7 @@ to_dir = "C:/WhiteHatJr/dowanloadedimages"
 list_of_files = os.listdir(from_dir)
 #print(list_of_files)
 
-# Move All Image files from Downloads Folder to Another Folder
+# Mueve todas las imagenes de la carpeta descargas a otra carpeta.
 for file_name in list_of_files:
 
     name, extension = os.path.splitext(file_name)
@@ -20,18 +20,18 @@ for file_name in list_of_files:
         continue
     if extension in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
 
-        path1 = from_dir + '/' + file_name                       # Example path1 : Downloads/ImageName1.jpg        
-        path2 = to_dir + '/' + "Image_Files"                     # Example path2 : D:/My Files/Image_Files      
-        path3 = to_dir + '/' + "Image_Files" + '/' + file_name   # Example path3 : D:/My Files/Image_Files/ImageName1.jpg
+        path1 = from_dir + '/' + file_name                       # Ejemplo path1 : Downloads/ImageName1.jpg        
+        path2 = to_dir + '/' + "Image_Files"                     # Ejemplo path2 : D:/My Files/Image_Files      
+        path3 = to_dir + '/' + "Image_Files" + '/' + file_name   # Ejemplo path3 : D:/My Files/Image_Files/ImageName1.jpg
         #print("path1 " , path1)
         #print("path3 ", path3)
 
-        # Check if Folder/Directory Path Exists Before Moving
-        # Else make a NEW Folder/Directory Then Move
+        # Verifica si la carptea/Directorio ruta existe antes de moverla
+        # De lo contrario, haz una NUEVA carpeta/Directorio luego muevela
         if os.path.exists(path2):
           print("Moving " + file_name + ".....")
 
-          # Move from path1 ---> path3
+          # Mueve de path1 ---> path3
           shutil.move(path1, path3)
 
         else:
